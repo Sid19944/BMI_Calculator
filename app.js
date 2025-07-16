039a9bb398e7541bf18a9cfd9d7972848ebc20ae
Siddharth Sarkar <sidsarkar2112@gmail.com>
2025-07-16 10:49:33 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let weight = document.querySelector("#weight")
+let height = document.querySelector("#height");
+let btn = document.querySelector("#Calculate");
+let bmi = document.querySelector("#bmi");
+let wCondition = document.querySelector("#w_condition");
+
+btn.addEventListener("click",function(){
+    let ans = ((weight.value)/((height.value/100)*(height.value/100)))
+    bmi.innerHTML = ans;
+    if(ans<19.1){
+        wCondition.innerHTML = "Weight Condition : Under Weight";
+    } else if(ans>19.1 && ans<27){
+        wCondition.innerHTML = "Weight Condition : Normal Weight";
+    } else {
+        wCondition.innerHTML = "Weight Condition : Over Weight";
+    }
+    setTimeout(()=>{
+        weight.value = "";
+        height.value = "";
+    },1500)
+})
